4006c4c17935a51d9d6984d8e1267e70818b1f3a
CynGL <cyntialino09@gmail.com>
2023-04-27 22:06:07 -0300

    atv

---
+++ app/src/main/java/br/com/etecia/moviens/Login_TangedcoActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.moviens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_TangedcoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_tangedco_layout);
+    }
+}
