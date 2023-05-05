e17d70a44003df869824a9a4635b7ca2278c9741
CynGL <cyntialino09@gmail.com>
2023-05-04 22:16:29 -0300

    bandio

---
+++ app/src/main/java/br/com/etecia/moviens/Bando_LoginActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.moviens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bando_LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bando_login_layout);
+    }
+}
